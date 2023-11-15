798ca17386e9a8b03f6b6aa73dc3eb56900b1622
Andrey Bondarenko <me@andreybondarenko.com>
2023-11-15 16:28:29 +0100

    Minecraft backup cli image

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Here I am keeping the dockerfiles I create or use for my projects.
 
  - PHP-FPM with everything the Wordpress and Nextcloud need. Non-minimal!
- - Postfix container, initial and dirty!
+ - Postfix container
  - Dovecot
  - Rspamd
  - Rsyslog
+ - \*-cli images are used for backups
--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 apps=( $(ls -la|grep drw|grep -v "\."| awk '{ print $9 }') )
 
-registry="registry.example.com"
+registry="registry.andreybondarenko.com"
 label="latest"
 
 for app in "${apps[@]}"
 do
   cd $app
-  docker build -t $registry/$app:label .
-  docker push $registry/$app:label
+  echo "-------- BUILDING $registry/$app:$label --------"
+  docker build -t $registry/$app:$label .
+  echo "-------- PUSHING $registry/$app:$label --------"
+  docker push $registry/$app:$label
   cd ..
 done
