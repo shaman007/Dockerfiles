c1d351bf2cd99c07abcb8e2bc1d22fc438396092
Andrey Bondarenko <me@andreybondarenko.com>
2025-04-14 14:05:38 +0200

    Add lables

--- build.sh
+++ build.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
-PATH=/root/bin:/usr/local/bin::/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-git pull
-apps=( $(ls -la|grep drw|grep -v "\."| awk '{ print $9 }') )
-registry="harbor.andreybondarenko.com"
-label="latest"
+set -euo pipefail
 
-for app in "${apps[@]}"
-do
-  cd $app
-  echo "-------- BUILDING $registry/$app:$label --------"
-  docker buildx build --platform linux/amd64,linux/arm64 -t $registry/library/$app:$label --push .  
+PATH=/root/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# Pull latest code
+git pull
+
+# Get timestamp
+timestamp=$(date +"%Y-%m-%d-%H%M")
+
+# Define variables
+apps=( $(ls -la | grep '^d' | grep -v '\.\{1,2\}$' | awk '{ print $9 }') )
+registry="harbor.andreybondarenko.com"
+
+# Loop through all app directories
+for app in "${apps[@]}"; do
+  echo "-------- BUILDING $registry/library/$app:latest and :$timestamp --------"
+  cd "$app"
+
+  docker buildx build --platform linux/amd64,linux/arm64 \
+    -t "$registry/library/$app:latest" \
+    -t "$registry/library/$app:$timestamp" \
+    --push .
+
   cd ..
 done
